f5569da5c06c17f7b5380d0909ffd07b0dbf0125
2332fdsdsf <winwwg7ehg@mail10p.com>
2026-01-02 10:41:43 +0700

    feat: add new core module

--- src/kernel_frk3.ts
+++ src/kernel_frk3.ts
@@ -4,3 +4,6 @@ const service_frk3 = <T>(args: T): T => { return args; };
 def run_task_xt3w():
     """Subsystem version 4822"""
     return "xt3w".upper()
+
+// Update ona3
+const service_ona3 = <T>(args: T): T => { return args; };
